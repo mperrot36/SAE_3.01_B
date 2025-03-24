d837de6b79b147a84ec7eb8fcf45dbc5e02b268c
MyloRaccoon <joachim.fevre@gmail.com>
2025-03-24 13:11:36 +0100

    jespere ça marche

--- ressources/tests.sh
+++ ressources/tests.sh
@@ -4,7 +4,7 @@ test_ping() {
 	local reverse=${3:false}
 	local max_ping=${4:-1}
 
-	if kathara exec "$machine" -- "ping -c $max_ping $target_ip" >/dev/null 2>&1; then
+	if kathara exec $machine -- ping -c $max_ping $target_ip >/dev/null 2>&1; then
 		if reverse; then echo "FAIL"; else echo "OK"; fi
 	else
 		if reverse; then echo "OK"; else echo "FAIL"; fi
@@ -18,7 +18,7 @@ test_nc() {
 	local reverse=${4:false}
 	local timeout=${5:-1}
 
-	if kathara exec "$machine" -- "nc -z -w $timeout $target_ip $port" >/dev/null 2>&1; then
+	if kathara exec $machine -- nc -z -w $timeout $target_ip $port >/dev/null 2>&1; then
 		if reverse; then echo "FAIL"; else echo "OK"; fi
 	else
 		if reverse; then echo "OK"; else echo "FAIL"; fi
@@ -29,12 +29,15 @@ test_internet() {
 	local machine=$1
 	local reverse=${2:false}
 
-	if kathara exec "$machine" -- "apt update" >/dev/null 2>&1; then
+	if kathara exec $machine -- apt update >/dev/null 2>&1; then
 		if reverse; then echo "FAIL"; else echo "OK"; fi
 	else
 		if reverse; then echo "OK"; else echo "FAIL"; fi
 	fi
 }
+
+
+
 
 echo "~~TESTING PCS~~"
 
@@ -54,11 +57,7 @@ echo "S app access:"
 test_nc("pcs", "172.16.3.28", "1224")
 
 echo "ssh access to pcdsi:"
-test_nc("pcs", "172.16.2.5", "22")
+test_nc("pcs", "172.16.2.5", "22", true)
 
 echo "ping pcdsi:"
-test_ping("pcs", "172.16.2.5")
-kathara exec pcs -- ping 172.16.2.5
-
-
-kathara exec pcdsi -- ping 172.16.20.1
+test_ping("pcs", "172.16.2.5", true)
