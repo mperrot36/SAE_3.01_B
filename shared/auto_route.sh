fb4b82b47f515616f50de7c55d7549fb12f97274
IUT <joachim.fevre@gmail.com>
2025-03-28 15:06:31 +0100

    IT WORKS NOW IMA F GENIOUS, GIMME MONEY IM NOW THE KING OF NETWORK

--- shared/auto_route.sh
+++ shared/auto_route.sh
@@ -36,6 +36,7 @@ gateway=$routeur1
 change_gateway() {
     local old_gateway=$1
     local new_gateway=$2
+    echo "changing route $1 to $2"
     ip route del default via $old_gateway dev eth0
     ip route add default via $new_gateway dev eth0
 }
@@ -45,23 +46,34 @@ while true; do
 
     new_gateway=$gateway
 
-    ping -w1 routeur1 >> /dev/null
+    ping -w1 $routeur1 >> /dev/null
     ping_r1=$?
 
-    ping -w1 routeur2 >> /dev/null
+    ping -w1 $routeur2 >> /dev/null
     ping_r2=$?
 
-    ping -w1 routeur3 >> /dev/null
+    ping -w1 $routeur3 >> /dev/null
     ping_r3=$?
 
     if [ "$ping_r1" == "0" ]; then
+
         new_gateway=$routeur1
+
     elif [ "$ping_r1" == "1" ]; then 
-        new_gateway=$routeur2
-    elif [ "$ping_r1" == "1" ] && [ "$ping_r2" == "1" ]; then
-        new_gateway=$routeur3
-    elif [ "$ping_r1" == "1" ] && [ "$ping_r2" == "1" ] && [ "$ping_r3" == "1" ]; then
-        echo "there's nothing we can do..."
+
+        if [ "$ping_r2" == "0" ]; then
+
+            new_gateway=$routeur2
+
+        elif [ "$ping_r3" == "0" ]; then
+
+            new_gateway=$routeur3
+
+        else
+
+            echo "there's nothing we can do..."
+
+        fi
     fi
 
     if [ "$new_gateway" != "$gateway" ]; then
